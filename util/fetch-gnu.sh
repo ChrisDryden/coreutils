509bdb653e8eb3c8c310f30ef4749783c30ed9e4
Christopher Dryden <christopher.paul.dryden@gmail.com>
2026-01-10 14:44:14 +0000

    fetch-gnu: remove fail-eperm.xpl and download latest fail-2eperm.sh

--- util/fetch-gnu.sh
+++ util/fetch-gnu.sh
@@ -3,6 +3,9 @@ ver="9.9"
 repo=https://github.com/coreutils/coreutils
 curl -L "${repo}/releases/download/v${ver}/coreutils-${ver}.tar.xz" | tar --strip-components=1 -xJf -
 
+# Remove tests that no longer exist on master
+rm -f tests/rm/fail-eperm.xpl
+
 # TODO stop backporting tests from master at GNU coreutils > 9.9
 curl -L ${repo}/raw/refs/heads/master/tests/mv/hardlink-case.sh > tests/mv/hardlink-case.sh
 curl -L ${repo}/raw/refs/heads/master/tests/mkdir/writable-under-readonly.sh > tests/mkdir/writable-under-readonly.sh
@@ -13,5 +16,6 @@ curl -L ${repo}/raw/refs/heads/master/tests/stty/bad-speed.sh > tests/stty/bad-s
 # Avoid incorrect PASS
 curl -L ${repo}/raw/refs/heads/master/tests/runcon/runcon-compute.sh > tests/runcon/runcon-compute.sh
 curl -L ${repo}/raw/refs/heads/master/tests/tac/tac-continue.sh > tests/tac/tac-continue.sh
+curl -L ${repo}/raw/refs/heads/master/tests/rm/fail-2eperm.sh > tests/rm/fail-2eperm.sh
 # Add tac-continue.sh to root tests (it requires root to mount tmpfs)
 sed -i 's|tests/split/l-chunk-root.sh.*|tests/split/l-chunk-root.sh\t\t\t\\\n  tests/tac/tac-continue.sh\t\t\t\\|' tests/local.mk
